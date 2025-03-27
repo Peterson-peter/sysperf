7d9f3cb5b303a310bb4ec638fef87ff828fb1a44
Pete <{ID}+{username}@users.noreply.github.com>
2025-03-27 14:42:50 -0700

    fix bug, make it easily runnable

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from disks_util import disk_util
 This program will automaticly run a full range of test using fio. Fio settings file can be found
 at /src/models/fio.yam. It will also send the output off disk. 
 """
+
+
+
+def main():
+
+    disks = disk_util.get_disks()
+    for disk in disks:
+        if "nvme" in disk:
+            fio.run(disk)
+
+
